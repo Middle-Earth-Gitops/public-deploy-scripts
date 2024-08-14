c3ff7c2967a79228262ee0e98ace72f02f15c07f
mthenry <mikethenry@proton.me>
2024-08-14 11:20:24 -0500

    Initial commit

---
+++ newproject.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#Version 1.8
+
+#Pull down github action file
+if [ ! -f .github/workflows/build.yaml ]; then
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/v8.3.0/build.yaml --create-dirs -o .github/workflows/build.yaml
+fi
+
+#Pull down docker-compose.yaml file
+if [ ! -f docker-compose.yaml ]; then
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/v8.3.0/docker-compose.yaml --create-dirs -o docker-compose.yaml
+fi
+
+#Pull down deploy-plan.json file
+if [ ! -f deploy-plan.json ]; then
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/v8.3.0/deploy-plan.json --create-dirs -o deploy-plan.json
+fi
+
+#give developers a script to create a new laravel project if a laravel app is not detected
+if [ ! -d app ]; then
+       if [ ! -f new-laravel-app.sh ]; then
+              curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/v8.3.0/new-laravel-app.sh --create-dirs -o new-laravel-app.sh
+              chmod +x new-laravel-app.sh
+       fi
+fi
+
+if [ ! -d Dockerfile.dev ]; then
+       curl -X POST -d @deploy-plan.json --header "Content-Type: application/json" -H "AUTH: $AUTH" http://localhost:8080/api/docker/build-dev > Dockerfile.dev
+fi
+
+#Build and run container
+docker stop app
+docker rm app
+docker-compose up -d --build
