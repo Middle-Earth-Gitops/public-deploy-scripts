cd2cf07378c058a99692e9b605107400bba975c8
mthenry <mikethenry@proton.me>
2025-07-01 10:06:10 -0500

    update

--- newproject.sh
+++ newproject.sh
@@ -11,7 +11,7 @@ while [ $# -gt 0 ]; do
     --new)
       provision_app=true
       ;;
-    --livewire|--no-livewire|--tailwind|--no-tailwind|--windows|--no-windows)
+    --livewire|--no-livewire|--pv|--tailwind|--no-tailwind|--ua-template|--no-ua-template|--windows|--no-windows)
       FORWARD_ARGS="$FORWARD_ARGS $1"
       ;;
     *)
@@ -24,34 +24,43 @@ done
 
 #Pull down github action file
 if [ ! -f .github/workflows/build.yaml ]; then
-       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/master/build.yaml --create-dirs -o .github/workflows/build.yaml
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/build.yaml --create-dirs -o .github/workflows/build.yaml
 fi
 
 #Pull down git pre-commit hook file
 if [ ! -f .git/hooks/pre-commit ]; then
-       curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/refs/tags/stable/laravel-hooks/pre-commit --create-dirs -o .git/hooks/pre-commit
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/laravel-hooks/pre-commit --create-dirs -o .git/hooks/pre-commit
 	   chmod +x .git/hooks/pre-commit
 fi
 
 #Pull down docker-compose.yaml file
 if [ ! -f docker-compose.yaml ]; then
-       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/master/docker-compose.yaml --create-dirs -o docker-compose.yaml
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/docker-compose.yaml --create-dirs -o docker-compose.yaml
 fi
 
 #Pull down deploy-plan.json file
 if [ ! -f deploy-plan.json ]; then
-       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/master/deploy-plan.json --create-dirs -o deploy-plan.json
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/deploy-plan.json --create-dirs -o deploy-plan.json
+fi
+
+# Pull and run add-pv.sh if the --pv flag is used.
+if echo "$FORWARD_ARGS" | grep -qw -- --pv; then
+       curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/add-pv.sh --create-dirs -o add-pv.sh
+       chmod +x add-pv.sh
+       ./add-pv.sh
+	   rm add-pv.sh
 fi
 
 #give developers a script to create a new laravel project if a laravel app is not detected
 if [ ! -d app ]; then
        if [ ! -f new-laravel-app.sh ]; then
-              curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/master/new-laravel-app.sh --create-dirs -o new-laravel-app.sh
+              curl https://raw.githubusercontent.com/Middle-Earth-Gitops/public-deploy-scripts/refs/tags/stable/new-laravel-app.sh --create-dirs -o new-laravel-app.sh
               chmod +x new-laravel-app.sh
        fi
 fi
 
-curl -X POST -d @deploy-plan.json --header "Content-Type: application/json" -H "AUTH: $AUTH" http://localhost:8080/api/docker/build-dev > Dockerfile.dev
+
+curl -X POST -d @deploy-plan.json --header "Content-Type: application/json" -H "AUTH: $AUTH" https://build-dockerfile-api.thewestgate.org/api/docker/build-dev > Dockerfile.dev
 
 #Build and run container
 docker stop app
